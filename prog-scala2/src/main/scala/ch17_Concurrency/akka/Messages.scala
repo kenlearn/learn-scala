355d54b2354b5c0ac7f605f2ab4d20ba80a8df07
Ken QCL <kenqcl@gmail.com>
2018-01-02 21:37:30 +0800

    update akka client

--- prog-scala2/src/main/scala/ch17_Concurrency/akka/Messages.scala
+++ prog-scala2/src/main/scala/ch17_Concurrency/akka/Messages.scala
@@ -10,7 +10,7 @@ object Messages {
     case class Create(key: Long, value: String) extends Request
     case class Read(key: Long) extends Request
     case class Update(key: Long, value: String) extends Request
-    case class Delte(key: Long) extends Request
+    case class Delete(key: Long) extends Request
 
     case class Response(result: Try[String])
 
